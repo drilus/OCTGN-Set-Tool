1106a252e82403d176c19cc421e5531d677050c7
drilus <mr.magus@gmail.com>
2013-03-29 22:05:36 -0400

    Initial Commit

---
+++ SetRenamer/frmMain.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader columnHeader2;
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "";
+            columnHeader2.Width = 210;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(484, 420);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Image";
+            this.columnHeader1.Width = 250;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 467);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(484, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Rename";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 502);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView1);
+            this.Name = "frmMain";
+            this.Text = "OCTGN IMG RENAMER";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ SetRenamer/frmMain.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmMain : Form
+    {
+        static string mtgDirectory;
+        static string[] imagePath;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            listView1.Items.Clear();
+            openFileDialog1.Filter = "OCTGN XML Image List (*.rels) | *.rels";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                mtgDirectory = openFileDialog1.FileName;
+                listView1.BeginUpdate();
+                using (XmlReader reader = XmlReader.Create(openFileDialog1.FileName))
+                {
+                    while (reader.Read())
+                    {
+                        if (reader.IsStartElement())
+                        {
+                            switch (reader.Name)
+                            {
+                                case "Relationship":
+                                    reader.MoveToFirstAttribute();
+                                    ListViewItem subitem = new ListViewItem();
+                                    subitem.Text = reader.Value;
+                                    reader.MoveToNextAttribute();
+                                    Guid id = new Guid();
+                                    Guid.TryParseExact(reader.Value.Substring(1), "N", out id);
+                                    subitem.SubItems.Add(id.ToString());
+                                    listView1.Items.Add(subitem);
+                                    break;
+                            }
+                        }
+                    }
+                }
+                listView1.EndUpdate();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = listView1.Items.Count;
+            progressBar1.Minimum = 0;
+            progressBar1.Step = 1;
+            foreach (ListViewItem item in listView1.Items)
+            {
+                string s = System.IO.Directory.GetParent(mtgDirectory).FullName;
+                imagePath = item.Text.Split('/');
+                s = System.IO.Directory.GetParent(s).FullName + "\\" + imagePath[1] + "\\";
+                string oldName = s + imagePath[2];
+                string extension = System.IO.Path.GetExtension(oldName);
+                string newName = s + item.SubItems[1].Text + extension;
+                try
+                {
+                    System.IO.File.Move(oldName, newName);
+                }
+                catch (System.IO.FileNotFoundException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                progressBar1.PerformStep();
+            }
+        }
+    }
+}
